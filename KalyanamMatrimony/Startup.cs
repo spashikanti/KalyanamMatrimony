0f1589a1a20b07582d09b159f7c57f843c3ff1b9
Sunil Kumar Pashikanti <36616898+spashikanti@users.noreply.github.com>
2023-08-14 19:38:50 +0000

    sln and setup files
    sln and setup files

--- KalyanamMatrimony/Startup.cs
+++ KalyanamMatrimony/Startup.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using KalyanamMatrimony.Models;
+﻿using KalyanamMatrimony.Models;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using System;
 
 namespace KalyanamMatrimony
 {
@@ -26,16 +23,43 @@ namespace KalyanamMatrimony
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.Configure<CookiePolicyOptions>(options =>
+            //{
+            //    options.CheckConsentNeeded = context => false;
+            //    options.MinimumSameSitePolicy = SameSiteMode.None;
+            //});
+            var emailConfig = _config
+                            .GetSection("EmailConfiguration")
+                            .Get<EmailConfiguration>();
+            services.AddSingleton(emailConfig);
+
             services.AddDbContextPool<AppDbContext>(options =>  
                 options.UseMySql(_config.GetConnectionString("MatrimonyDBConnection"))
             );
-            
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<AppDbContext>();
 
+            services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.SignIn.RequireConfirmedEmail = true;
+            })
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+
+            ////To change the default access denied route
+            //services.ConfigureApplicationCookie(options =>
+            //{
+            //    options.AccessDeniedPath = new PathString("/Administration/AccessDenied");
+            //});
+
+            //services.AddHttpContextAccessor();
             services.AddMvc().AddXmlSerializerFormatters();
-            services.AddScoped<IEmployeeRepository, SQLEmployeeRepository>();
             services.AddScoped<IMatrimonyRepository, MySqlMatrimonyRepository>();
+            services.AddScoped<IEmailSender, EmailSender>();
+
+            services.AddDistributedMemoryCache(); // Adds a default in-memory implementation of IDistributedCache
+            services.AddSession(options => {
+                options.IdleTimeout = TimeSpan.FromMinutes(20);
+            });
+            services.AddProgressiveWebApp();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,9 +69,16 @@ namespace KalyanamMatrimony
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseStatusCodePagesWithReExecute("/Error/{0}");
+                app.UseExceptionHandler("/Error");
+            }
 
             app.UseStaticFiles();
             app.UseAuthentication();
+            app.UseCookiePolicy();
+            app.UseSession();
 
             app.UseMvc(routes =>
             {
